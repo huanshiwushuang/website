d6a9ff3b0d80eec3cedb3fbc5550f4e407b7ea20
guohao <1403902918@qq.com>
2018-05-23 00:08:18 +0800

    于0xEF的单个字节在单字节编码下被解析成

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-var path = require('path');
-console.log(path.extname('asd.js/pasd.txt'));
+var fs = require('fs'),
+	path = require('path');
+
+function replace(pathname) {
+	var str = fs.readFileSync(pathname, 'binary');
+	console.log(fs.readFileSync(pathname));
+	console.log(str);
+	str = str.replace('gbk', 'hahaha');
+	console.log(str);
+	fs.writeFileSync(pathname, 'binary');
+}
+
+replace('./gbk.txt');
