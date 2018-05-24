f90145d510916ad8bbe37e15269bb5557b638cce
guohao <1403902918@qq.com>
2018-05-24 15:00:04 +0800

    七天学会NodeJS 看完了

--- server.js
+++ server.js
@@ -1,16 +1,108 @@
-var net = require('net');
+var http = require('http'),
+	fs = require('fs'),
+	path = require('path');
 
-net.createServer(function (conn) {
-	// 接收到连接，监听连接的数据
-	conn.on('data', function (data) {
-		conn.write([
-			'HTTP/1.1 200 OK',
-			'Content-Type: text/plain',
-			'Content-Length: 11',
-			'',
-			'Hello World'
-		].join('\n'));
+var MIME = {
+	'.css': 'text/css',
+	'.js': 'application/javascript'
+};
 
-		console.log(data.toString());
+main(process.argv.slice(2));
+
+function main(argv) {
+	var config = JSON.parse((argv[0] && fs.readFileSync(argv[0], 'utf-8')) || '{}'),
+		root = config.root || '.',
+		port = config.port || 80,
+		server;
+
+	server = http.createServer(function (req, res) {
+		var pathInfo = parseUrl(root, req.url);
+
+		if (pathInfo.err) {
+			res.writeHead(404);
+			res.end(pathInfo.err.message);
+			console.log(pathInfo.err.message);
+		} else {
+			validateFiles(pathInfo.pathnames, function (err,pathnames) {
+				if (err) {
+					res.writeHead(404);
+					res.end(err.message);
+				} else {
+					res.writeHead(200, {
+						'Content-Type': pathInfo.mime
+					})
+					outputFile(pathnames,res);
+				}
+			})
+			
+		}
+	}).listen(port);
+
+	process.on('SIGTERM', function () {
+		server.close(function () {
+			process.exit(0);
+		})
 	})
-}).listen(8080);
+}
+function validateFiles(pathnames, callback) {
+	(function next(i, len) {
+		if (i < len) {
+ 			fs.stat(pathnames[i], function (err, stats) {
+ 				if (err) {
+ 					callback(err);
+ 				} else if (!stats.isFile()) {
+ 					callback(new Error('Not File'));
+ 				} else {
+ 					next(i+1, len);
+ 				}
+ 			})
+		} else {
+			callback(null, pathnames);
+		}
+	})(0,pathnames.length)
+}
+function outputFile(pathnames, writer) {
+	(function next(i,len) {
+		if (i < len) {
+			let reader = fs.createReadStream(pathnames[i]);
+			reader.pipe(writer, {end: false});
+			reader.on('end', function () {
+				next(i+1, len);
+			})
+		} else {
+			writer.end();
+		}
+	})(0,pathnames.length)
+}
+function parseUrl(root, url) {
+	console.log(url);
+	var base,parts,pathnames;
+
+	if (url.indexOf('??') != -1) {
+		url = url.replace('/', '/??');
+	}
+	parts = url.split('/??');
+	try {
+		base = path.join('/',parts[1]);
+	} catch (e) {
+
+		return {
+			err: e
+		}
+	}
+	pathnames = parts[2].split(',').map(function (item) {
+		return path.join(root,base,item);
+	})
+
+	return {
+		mime: MIME[path.extname(pathnames[0])] || 'text/plain',
+		pathnames: pathnames
+	}
+}
+
+
+
+
+// http://assets.example.com/foo/??bar.js,baz.js
+// http://assets.example.com/foo/bar.js
+
