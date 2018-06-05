b5fc9c3a5acf7c26419b48921eb9d33c9368c97e
guohao <1403902918@qq.com>
2018-06-06 00:28:10 +0800

    到公司

--- talk-room/index.js
+++ talk-room/index.js
@@ -5,7 +5,16 @@ var nowCount = 0;
 // 创建服务器
 
 var server = net.createServer(function (conn) {
-	console.log('新的连接收到');
+	conn.write(
+		'\n > welcome to \x1b[92m talk-room \x1b[39m'
+		+'\n > '
+		+(++nowCount)
+		+' other people are connected at this time.'
+		+'\n > please write your name and press enter:'
+	);
+	conn.on('close', function () {
+		nowCount--;
+	})
 })
 
 // 监听端口
