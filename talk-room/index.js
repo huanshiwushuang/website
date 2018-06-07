e6c308e2efc7ad49d51cac1bee62d9cf65f264c9
guohao <1403902918@qq.com>
2018-06-07 08:34:20 +0800

    看到了97页，创建irc客户端，但是要学习VUE了  暂时停止node学习，千万不能忘记啊啊啊啊啊啊啊啊啊啊啊啊

--- talk-room/index.js
+++ talk-room/index.js
@@ -6,38 +6,45 @@ var nowCount = 0,
 // 创建服务器
 
 var server = net.createServer(function (conn) {
-	conn.setEncoding('utf8');
+	var nickname;
 	nowCount++;
 
-	conn.write('\nsuccess, you are number: '+nowCount);
-	conn.write('\nplease enter your nickname：');
+	conn.write('\r\nsuccess, you are number: '+nowCount);
+	conn.write('\r\nplease enter your nickname：');
 	conn.on('data', function (data) {
-		data = data.replace('\r\n','');
-		if (users[data]) {
-			conn.write('\nnickname already exists, please enter your nickname：');
-		} else {
-			// 新用户登录
-			for (let i in users) {
-				i.write('user '+data+' login');
+		let tmp;
+		data = Buffer.from(data.join(',').replace(/,?(10|13)\b/g,'').split(','));
+		tmp = data.join(',');
+		if (nickname) {
+			if (data.toString() == 'getNickName') {
+				conn.write('\x1b[32m your nickname is '+Buffer.from(nickname.split(','))+'\x1b[0m');
+			} else {
+				conn.write('\r\nyour input is '+data);
 			}
-			users[conn] = {};
-			users[conn].nickname = data;
+		} else {
+			if (users[tmp]) {
+				conn.write('\r\n\x1b[31mnickname already exists, please enter your nickname：');
+			} else {
+				nickname = tmp;
+				// 新用户登录，广播所有在线用户
+				for (let i in users) {
+					users[i].conn.write('\r\nnew user '+data+' login!');
+				}
+				users[nickname] = {};
+				users[nickname].conn = conn;
+			}
 		}
-		console.log(data);
-	})
-	conn.on('end', function (data) {
-		console.log(data);
 	})
 	conn.on('error', function (error) {
 		console.log('\n\x1b[31;47mError Info:\x1b[0m');
 		console.log(`\t${error}\n`);
 	})
 	conn.on('close', function () {
-		debugger
-		console.log('connection disconnect，current number：'+(--nowCount));
+		console.log('connection disconnect, current number：'+(--nowCount));
+		// 断开的时候，删除保存的用户
+		delete users[nickname]
 	})
 })
-
 // 监听端口
 server.listen(3000, function () {
 	console.log('正在监听端口 3000');
